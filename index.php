a8cc5c57d0f9189a660dd6552de5449691590e1e
Antonio Pierro <antonio.pierro@gmail.com>
2016-06-07 21:45:56 +0200

    Added Sign in and Log out html link

--- index.php
+++ index.php
@@ -6,3 +6,21 @@ $db = new DB;
 $googleClient = new Google_Client;
 
 $auth = new GoogleAuth($db, $googleClient);
+
+$authUrl = 1; // TODO - change me
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Google Authentication Test</title>
+    </head>
+    <body>
+        <?php if($authUrl): ?>
+            <a href="<?=$authUrl?>">Sign in with Google</a>
+        <?php else: ?>
+            >You are logged in.</a><a href="logout.php">Log out</a>
+        <?php endif;?>
+    </body>
+</html>
